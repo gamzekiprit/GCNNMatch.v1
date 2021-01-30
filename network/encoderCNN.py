f1d62acd79cc0c764e2e578ca63cc8320639d1c6
Fabian Herzog <fabianfubel@gmail.com>
2021-01-30 18:10:20 +0100

    :art: Remove DataParallel.

--- network/encoderCNN.py
+++ network/encoderCNN.py
@@ -7,8 +7,8 @@ class EncoderCNN(nn.Module):
     def __init__(self, embed_size=512):
         super(EncoderCNN, self).__init__()
         # get the pretrained densenet model
-        initial_cnn = models.densenet121(pretrained=True)
-        # initial_cnn = models.resnet18(pretrained=True)
+        # initial_cnn = models.densenet121(pretrained=True)
+        initial_cnn = models.resnet18(pretrained=True)
         self.cnn = torch.nn.Sequential(*(list(initial_cnn.children())[:-1]))
         # Freeze model weights
         #print(self.cnn.parameters())
